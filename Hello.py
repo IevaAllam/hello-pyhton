6a6cf954ddc3919862cfd44ca9185e35868c9045
IevaAllam <ieva.urbonaviciute2010@gmail.com>
2025-10-02 18:05:39 +0300

    hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,10 @@
-print("Hello, world! Ieva learning Python")
+# hello.py
+# My very first Python script 🚀
+
+def main():
+    print("Hello, world!")
+    print("👋 I'm Ieva, starting my Python & ML journey.")
+    print("Let's learn step by step and build great projects!")
+
+if __name__ == "__main__":
+    main()
